8e8c40d23fe0534ca063686675da0969cba759f9
Thomas <Touhma@outlook.com>
2024-05-27 13:22:26 -0400

    Update to add initialisation generator

--- Assets/Scripts/Commons/Architectures/Systems/EntitiesFactoryInitializationSystem.cs
+++ Assets/Scripts/Commons/Architectures/Systems/EntitiesFactoryInitializationSystem.cs
@@ -4,11 +4,8 @@ using Unity.Entities;
 namespace Commons.Architectures {
     
     // Need to be codegen 
-    [UpdateInGroup(typeof(Unity.Entities.InitializationSystemGroup))]
+   // [UpdateInGroup(typeof(Unity.Entities.InitializationSystemGroup))]
     public partial struct EntitiesFactoryInitializationSystem : ISystem {
-        public void OnCreate(ref SystemState state) { // Need to setup the factory once 
-            EntityFactories.PlayerEntityFactorySetup(ref state); 
-            state.Enabled = false;
-        }
+        
     }
 }
