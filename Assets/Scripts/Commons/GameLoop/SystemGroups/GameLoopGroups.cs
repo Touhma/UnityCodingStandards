358e135185b6ce23c074759058dc89e07fd30818
Thomas <Touhma@outlook.com>
2024-05-28 18:33:55 -0400

    plop

---
+++ Assets/Scripts/Commons/GameLoop/SystemGroups/GameLoopGroups.cs
@@ -0,0 +1,57 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+// using Unity.Physics.Systems;
+
+namespace Commons.GameLoop
+{
+    //Base Game System Groups
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateBefore(typeof(GameLoopPreTransformSystemGroup))]
+    public partial class GameLoopSystemGroup : ComponentSystemGroup { } // The gameloop execute BEFORE the transform Update
+
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateAfter(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(TransformSystemGroup))]
+    public partial class GameLoopPreTransformSystemGroup : ComponentSystemGroup { } // The gameloop execute BEFORE the transform Update
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(GameLoopServicesInitialisationGroup))]
+    public partial class GameLoopSingletonsInitialisationGroup : ComponentSystemGroup { }
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(GameLoopDatabasesInitialisationGroup))]
+    public partial class GameLoopServicesInitialisationGroup : ComponentSystemGroup { }
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(GameLoopStatesInitialisationGroup))]
+    public partial class GameLoopDatabasesInitialisationGroup : ComponentSystemGroup { }
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(GameLoopInputGroup))]
+    public partial class GameLoopStatesInitialisationGroup : ComponentSystemGroup { }
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(GameLoopPostInputGroup))]
+    public partial class GameLoopInputGroup : ComponentSystemGroup { } // Non physics related inputs
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(GameLoopPreGameplayGroup))]
+    public partial class GameLoopPostInputGroup : ComponentSystemGroup { } // Consumation of inputs state to prepare gameplay state
+
+    /*
+    [UpdateInGroup(typeof(AfterPhysicsSystemGroup))]
+    public partial class GameLoopPhysicsInputGroup : ComponentSystemGroup { } // Raycast & co
+    */
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    [UpdateBefore(typeof(GameLoopGameplayGroup))]
+    public partial class GameLoopPreGameplayGroup : ComponentSystemGroup { } // Everything else
+
+    [UpdateInGroup(typeof(GameLoopSystemGroup))]
+    public partial class GameLoopGameplayGroup : ComponentSystemGroup { } // Everything else
+
+
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    public partial class GameLoopPresentationGroup : ComponentSystemGroup { } // UI & stuff
+}
