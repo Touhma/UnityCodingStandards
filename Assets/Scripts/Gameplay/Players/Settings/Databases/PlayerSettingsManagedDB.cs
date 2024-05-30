818809c223a19803245f61fe8837370c0e0188d6
Thomas <Touhma@outlook.com>
2024-05-30 18:15:39 -0400

    Fix Managed Settings Shortcuts

--- Assets/Scripts/Gameplay/Players/Settings/Databases/PlayerSettingsManagedDB.cs
+++ Assets/Scripts/Gameplay/Players/Settings/Databases/PlayerSettingsManagedDB.cs
@@ -31,4 +31,29 @@ namespace Players.Settings.Databases {
             return Settings.TryGetValue(typeof(T), out ScriptableObject setting) ? setting as T : null;
         }
     }
+    
+    [UsedImplicitly]
+    [ManagedSetting] // This will create a ManagedSettings shortcut so you can call : ManagedSettings.PlayerSettingsManagedDB directly
+    [InitializeInGroup(typeof(SettingsServiceGroup))] // use the ServiceGroup attributes for initialization of services or else it's not gonna register
+    public class PlayerSettingsManagedDB2 : IServiceBase {
+        public readonly Dictionary<Type, ScriptableObject> Settings = new();
+
+        public string SettingPath = "Settings/Players";
+
+        public void Initialize() {
+            Debug.Log("Initialize -> " + SettingPath + " -> " + Resources.LoadAll<ScriptableObject>(SettingPath).Length);
+
+            foreach (ScriptableObject scriptableObject in Resources.LoadAll<ScriptableObject>(SettingPath)) {
+                Settings.Add(scriptableObject.GetType(), scriptableObject);
+            }
+        }
+
+        public void Destroy() {
+            Settings.Clear();
+        }
+
+        public T GetSettings<T>() where T : ScriptableObject {
+            return Settings.TryGetValue(typeof(T), out ScriptableObject setting) ? setting as T : null;
+        }
+    }
 }
